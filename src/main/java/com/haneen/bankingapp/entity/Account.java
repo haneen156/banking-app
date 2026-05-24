04e8d568189f96048a1fcf015bcd0918e0e967c1
Haneen Hazem <haneen1802.hazem@gmail.com>
2026-05-25 01:21:57 +0300

    Added Account entity and JPA repository

---
+++ src/main/java/com/haneen/bankingapp/entity/Account.java
@@ -0,0 +1,44 @@
+package com.haneen.bankingapp.entity;
+
+import jakarta.persistence.*;
+
+@Table(name = "accounts")
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "account_holder_name")
+    private String accountHolderName;
+    private double balance;
+
+    public Account(){
+    }
+
+    public Account(String accountHolderName, double balance) {
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public void setAccountHolderName(String accountHolderName) {
+        this.accountHolderName = accountHolderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
