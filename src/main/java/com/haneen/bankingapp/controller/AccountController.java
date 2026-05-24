dcc1aebb11c4985f0630db38adc3b53afdb07358
Haneen Hazem <haneen1802.hazem@gmail.com>
2026-05-25 01:34:39 +0300

    Added account post and getById REST endpoints

---
+++ src/main/java/com/haneen/bankingapp/controller/AccountController.java
@@ -0,0 +1,31 @@
+package com.haneen.bankingapp.controller;
+
+import com.haneen.bankingapp.dto.AccountDto;
+import com.haneen.bankingapp.service.AccountService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+
+    private AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    //ADD new Account
+    @PostMapping
+    public ResponseEntity<AccountDto> addAccount(@RequestBody AccountDto accountDto){
+        return new ResponseEntity<>(accountService.createAccount(accountDto), HttpStatus.CREATED);
+    }
+
+    //find account by id - get
+    @GetMapping("/{id}")
+    public ResponseEntity<AccountDto> getAccountById(@PathVariable Long id){
+        AccountDto accountDto = accountService.getAccountById(id);
+        return ResponseEntity.ok(accountDto);
+    }
+}
